826f88abdc678c50df4499522ed691495f5c1758
James Chambers <>
2019-12-07 13:10:05 -0800

    updated

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
 // title is destructured from props.title
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div
-      className={`${size} menu-item`}>
+      className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div 
         className='background-image'
         style={{backgroundImage: `url(${imageUrl})`}}      
@@ -15,5 +17,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>      
   </div>
 );
-
-export default MenuItem;
+// returns super powered component with history, location, and match 
+export default withRouter(MenuItem);
